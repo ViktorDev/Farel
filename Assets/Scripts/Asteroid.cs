4174743b0328d87ff90735be76790d35d1e89401
maxbaranuk <maxbaranuk@gmail.com>
2016-03-30 15:26:45 +0300

    Add game scripts

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Moon") {
+
+        }
+        
+    }
+}
--- Assets/Scripts/GameScene.cs
+++ Assets/Scripts/GameScene.cs
@@ -4,11 +4,12 @@ using System.Collections;
 public class GameScene : MonoBehaviour {
 
     public GameObject asteroid;
-    bool isGame;
+    bool isGame = true;
     public float spawnAsteroidTime = 1f;
+    public GameObject moon;
 	// Use this for initialization
 	void Start () {
-	
+        StartCoroutine(spawnAsteroid());
 	}
 	
 	// Update is called once per frame
@@ -18,7 +19,11 @@ public class GameScene : MonoBehaviour {
 
     IEnumerator spawnAsteroid() {
         while (isGame) {
-            Instantiate(asteroid, new Vector3(), Quaternion.identity);
+            Vector3 dir = new Vector3(Random.Range(-1f,1f), Random.Range(0.1f,1f), Random.Range(-1f,1f));
+            dir.Normalize();
+            dir *= 25;
+            GameObject ast = (GameObject) Instantiate(asteroid, dir, Quaternion.identity);
+            ast.GetComponent<Rigidbody>().AddForce((moon.transform.position - dir)*0.3f, ForceMode.Impulse);
             yield return new WaitForSeconds(spawnAsteroidTime);
         }
 
