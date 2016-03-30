57b1fc9ce77d19c6d1d480798ff8ac5b70348041
maxbaranuk <maxbaranuk@gmail.com>
2016-03-30 14:46:53 +0300

    MainScene

---
+++ Assets/Scripts/GameScene.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScene : MonoBehaviour {
+
+    public GameObject asteroid;
+    bool isGame;
+    public float spawnAsteroidTime = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator spawnAsteroid() {
+        while (isGame) {
+            Instantiate(asteroid, new Vector3(), Quaternion.identity);
+            yield return new WaitForSeconds(spawnAsteroidTime);
+        }
+
+    }
+}
