f9a12ab1c78cde817b3d80ce67675bb26207b155
maxbaranuk <maxbaranuk@gmail.com>
2016-03-31 16:09:34 +0300

    Add buisnessManager

--- Assets/Scripts/BuisnessView.cs
+++ Assets/Scripts/BuisnessView.cs
@@ -1,16 +1,70 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
 
 public class BuisnessView : MonoBehaviour 
 {
+    public static BuisnessView instance;
+    public GameObject lotPanel;
+    public GameObject lotInfo;
+    public Material onSale;
+    public Material saled;
+    public GameObject selectedLot;
 
 	void Start () 
 	{
-	
-	}
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
 	
 	void Update () 
 	{
-	
-	}
+        UserInput();
+    }
+
+    void UserInput()
+    {
+#if UNITY_ANDROID
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+                if (hit.transform.gameObject.tag == "Lot")
+                {
+                    OpenLotInfo(hit.transform.gameObject);
+                    selectedLot = hit.transform.gameObject;
+                }
+        }
+#endif
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray, out hit, 100))
+                if (hit.transform.gameObject.tag == "Lot")
+                {
+                    Debug.Log("Select");
+                    OpenLotInfo(hit.transform.gameObject);
+                    selectedLot = hit.transform.gameObject;
+                }
+        }
+#endif
+   } 
+
+    void OpenLotInfo(GameObject lot) {
+        lotPanel.SetActive(true);
+        lotPanel.GetComponent<BuyLotPanel>().OpenPanel();
+        lotInfo.GetComponent<Text>().text = "Area: 1000\nEnergy: yes\nWater: yes\nOther: Coal";
+    }
+
+    public void BuyLot() {
+        selectedLot.GetComponent<MeshRenderer>().material = saled;
+        selectedLot.tag = "SaledLot";
+        lotPanel.GetComponent<BuyLotPanel>().ClosePanel();
+    }
 }
---
+++ Assets/Scripts/BuyLotPanel.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuyLotPanel : MonoBehaviour {
+
+      public RectTransform panel;
+ //   GameObject panel;
+	// Use this for initialization
+	void Start () {
+ //       panel = GetComponentInChildren<RectTransform>();
+    }
+
+    //void Awake() {
+    //    OpenPanel();
+    //}
+
+    public void OpenPanel() {
+        StartCoroutine(Open());
+    }
+
+    public void ClosePanel() {
+        StartCoroutine(Close());
+    }
+
+    IEnumerator Open() {
+        while (panel.localScale.y < 1) {
+            panel.localScale = new Vector3(1, panel.localScale.y+0.05f, 1);
+            yield return new WaitForSeconds(0.01f);
+        }
+        panel.localScale = new Vector3(1, 1, 1);
+    }
+
+    IEnumerator Close()
+    {
+        while (panel.localScale.y>0)
+        {
+            panel.localScale = new Vector3(1, panel.localScale.y - 0.05f, 1);
+            yield return new WaitForSeconds(0.01f);
+        }
+        panel.localScale = new Vector3(1, 0, 1);
+        gameObject.SetActive(false);
+    }
+}
