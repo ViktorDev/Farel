7996e26596ffc822faa61a8b8dc3232eeb8646f4
ViktorDev <viktor.marchuk2205@gmail.com>
2016-03-31 12:02:03 +0300

    add moon movement script

---
+++ Assets/Scripts/MoonMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoonMovement : MonoBehaviour 
+{
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+		transform.Rotate(Vector3.up * Time.deltaTime * 3);
+	}
+}
