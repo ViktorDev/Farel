ddff25164f381e6e5d0ecd28d6c88c2cf76b88e4
maxbaranuk <maxbaranuk@gmail.com>
2016-04-04 17:31:38 +0300

    Add shadow to moon

--- Assets/Scripts/MoonMovement.cs
+++ Assets/Scripts/MoonMovement.cs
@@ -4,13 +4,18 @@ using System.Collections;
 public class MoonMovement : MonoBehaviour 
 {
 
+    GameObject arCam;
+    GameObject shadow;
 	void Start () 
 	{
-	
+        arCam = GameObject.Find("ARCamera");
+        shadow = transform.Find("Shadow").gameObject;
 	}
 	
 	void Update () 
 	{
 		transform.Rotate(Vector3.up * Time.deltaTime * 3);
-	}
+        shadow.transform.rotation = Quaternion.LookRotation(transform.position - arCam.transform.position);
+
+    }
 }
