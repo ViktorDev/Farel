01517e7552f6929a39fc8dea8eae9a6259cf7a0d
ViktorDev <viktor.marchuk2205@gmail.com>
2016-04-06 11:16:09 +0300

    add movie player

---
+++ Assets/Scripts/PlayMovie.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMovie : MonoBehaviour 
+{
+	//public string clipName;
+	
+	public void PlayClip()
+	{
+		StartCoroutine (Start ());
+	}
+
+	IEnumerator Start()
+	{
+
+		Handheld.PlayFullScreenMovie ("Unity 5 Launch Trailer.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+
+		yield return null;
+
+		print ("Move Scene");
+	}
+}
