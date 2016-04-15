b700732c9caa561ba36950a530be048f2f1be4c1
maxbaranuk <maxbaranuk@gmail.com>
2016-04-15 19:01:11 +0300

    Add basket scene

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    GameObject cam;
+    public bool isStay = true;
+    Vector2 startPos;
+    Vector2 endPos;
+    Vector3 ballDir;
+    GameManager manager;
+	// Use this for initialization
+	void Start () {
+        cam = GameObject.Find("ARCamera");
+
+        manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.z < -15) Destroy(gameObject);     
+        if (isStay ) shotListener();       
+    }
+
+    void OnTriggerEnter(Collider otherCollider)
+    {
+        if (otherCollider.gameObject.tag == "winDetector")
+        {
+            manager.addPoints();
+        }
+    }
+    void shotListener()
+    {
+//        if (Input.GetKeyDown("space")) makeShot(new Vector2(50, 450));
+        
+
+        if (Input.touchCount > 0 & !manager.isFinishGame)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began) startPos = touch.position;
+            
+            if (touch.phase == TouchPhase.Moved)
+            {
+                float hor = 3 * touch.deltaPosition.x;
+                float vert = 3 * touch.deltaPosition.y;
+                transform.RotateAround(transform.position, cam.transform.Find("Camera").right * vert, 200 * Time.deltaTime);
+                transform.RotateAround(transform.position, -cam.transform.Find("Camera").up * hor, 200 * Time.deltaTime);
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                
+                endPos = touch.position;
+                Vector2 dir = endPos - startPos;
+                if (dir.y > 50) makeShot(dir);              
+            }
+        }
+    }
+
+    void makeShot(Vector2 dir)
+    {
+        manager.totalShots++;
+        isStay = false;
+        
+        gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        gameObject.GetComponent<Rigidbody>().useGravity = true;
+
+        ballDir = cam.transform.Find("Camera").forward;
+        ballDir.z = ballDir.z + 0.5f;
+        gameObject.transform.parent = null;
+        gameObject.GetComponent<Rigidbody>().AddForce(ballDir * dir.y / 12, ForceMode.Impulse);
+        gameObject.GetComponent<Rigidbody>().AddTorque(cam.transform.Find("Camera").right * dir.y / 4);
+        gameObject.GetComponent<Rigidbody>().AddTorque(-cam.transform.Find("Camera").up * dir.x / 4);
+        manager.createNewBall();
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+//using Facebook.Unity;
+//using System.Collections.Generic;
+//using System;
+//using UnityEngine.Advertisements;
+
+public class GameManager : MonoBehaviour
+{
+   
+    public int totalShots = 0;
+    int goalShots = 0;
+    int points;
+    public GameObject arCam;
+//    bool isShared;
+//    bool isLoginned;
+    public bool isFinishGame;
+    public float timeToFinish;
+    public GameObject ball;
+    // Use this for initialization
+    void Start () {
+    }
+
+    void OnEnable() {
+        createNewBall();
+    }
+
+    void Update () {
+       
+        if (timeToFinish < 0) {
+            timeToFinish = 0;           
+            isFinishGame = true;
+           
+            int perc = (int)(((float) goalShots / totalShots) * 100);
+        }
+    }
+
+    public void addPoints() { 
+      goalShots++;
+      points += 2;
+    }
+
+    public void createNewBall() {
+        StartCoroutine(createBall());
+    }
+
+    IEnumerator createBall() {
+        yield return new WaitForSeconds(0.4f);
+            GameObject newBall = Instantiate(ball);
+            newBall.gameObject.transform.parent = arCam.transform;
+            newBall.GetComponent<Rigidbody>().isKinematic = true;
+            newBall.transform.localPosition = new Vector3(0, -2f, 5f);
+            newBall.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    public void startNewGame() {
+        StartCoroutine(startgame());        
+    }
+
+    public void startGame() {
+        createNewBall();
+    }
+
+    IEnumerator startgame() {
+        yield return new WaitForSeconds(0.3f);
+        goalShots = 0;
+        points = 0;
+        totalShots = 0;
+        timeToFinish = 60;
+        isFinishGame = false;
+    }
+}
